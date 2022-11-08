e23208d7a1f926431c2adf2848d426ea2c83bd45
Mu Li <mli@amazon.com>
2022-11-08 12:02:29 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     name='autocut',
     version=__version__,
     install_requires=requirements,
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
