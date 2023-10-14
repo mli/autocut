d8f7232d88c3ee17159db6bc856ed198eaeaf6b8
chenqianhe <1278095698@qq.com>
2023-10-14 21:39:20 +0800

    ci: update ci

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 requirements = [
     "ffmpeg-python",
     "moviepy",
-    "openai",
     "openai-whisper",
     "opencc-python-reimplemented",
     "parameterized",
@@ -17,6 +16,11 @@ requirements = [
 setup(
     name="autocut",
     install_requires=requirements,
+    extras_require={
+        "all": ["openai", "faster-whisper"],
+        "openai": ["openai"],
+        "faster": ["faster-whisper"],
+    },
     packages=find_packages(),
     entry_points={
         "console_scripts": [
