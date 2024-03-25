b84dfa36862fad8d034910c7a89ba3ed05b9164b
chenqianhe <1278095698@qq.com>
2024-03-25 15:53:22 +0800

    格式化代码

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     install_requires=requirements,
     url="https://github.com/mli/autocut",
     project_urls={
-      "source": "https://github.com/mli/autocut",
+        "source": "https://github.com/mli/autocut",
     },
     license="Apache License 2.0",
     long_description=open("README.md").read(),
